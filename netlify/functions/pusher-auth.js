9d79bf2f1c95b7546eff7f5699f2f85052e30c12
0Azuree <ddwip00@gmail.com>
2025-06-05 21:06:35 -0400

    Update pusher-auth.js

--- netlify/functions/pusher-auth.js
+++ netlify/functions/pusher-auth.js
@@ -1,7 +1,6 @@
 // netlify/functions/pusher-auth.js
 const Pusher = require('pusher');
 
-// Initialize Pusher (ensure these are set as Netlify Environment Variables)
 const pusher = new Pusher({
     appId: process.env.PUSHER_APP_ID,
     key: process.env.PUSHER_APP_KEY,
@@ -14,27 +13,19 @@ exports.handler = async function(event, context) {
     if (event.httpMethod !== 'POST') {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
-
     const socketId = event.body.split('&')[0].split('=')[1];
     const channelName = event.body.split('&')[1].split('=')[1];
-
-    // Extract userId and username from custom headers
     const userId = event.headers['x-user-id'];
     const username = event.headers['x-username'];
 
     if (!userId || !username) {
         return { statusCode: 401, body: 'Unauthorized: Missing user ID or username headers' };
     }
-
     try {
-        // Authorize private channels
         const authResponse = pusher.authorizeChannel(socketId, channelName, {
             user_id: userId,
-            user_info: {
-                username: username
-            }
+            user_info: { username: username }
         });
-
         return {
             statusCode: 200,
             headers: { 'Content-Type': 'application/json' },
